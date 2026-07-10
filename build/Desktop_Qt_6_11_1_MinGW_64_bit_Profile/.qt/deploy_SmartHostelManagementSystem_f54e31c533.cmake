35121c77219ac3ea71f0481d382d209db23b3e5e
Kushagralpu2024 <shivvindkumar9906@gmail.com>
2026-07-10 15:56:49 +0530

    Remove build artifacts and IDE files

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Profile/.qt/deploy_SmartHostelManagementSystem_f54e31c533.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/shivv/Downloads/SmartHostelManagementSystem/build/Desktop_Qt_6_11_1_MinGW_64_bit_Profile/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/SmartHostelManagementSystem-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/shivv/Downloads/SmartHostelManagementSystem/build/Desktop_Qt_6_11_1_MinGW_64_bit_Profile/SmartHostelManagementSystem.exe"
-    GENERATE_QT_CONF
-)
